72b5e7db6ca2bf70acf5aa6a2897a67c66bce8ef
John Fremlin <john@fremlin.org>
2009-06-13 03:26:00 +0000

    improve quickstart to actually start the event loop

--- quickstart.lisp
+++ quickstart.lisp
@@ -5,7 +5,6 @@
 (eval-when (:compile-toplevel :execute :load-toplevel)
   (require 'asdf-install))
 
-
 (handler-bind (((or asdf-install::key-not-found asdf-install::download-error asdf-install::no-signature) 
 		(lambda(c) (declare (ignore c)) (invoke-restart 'asdf-install::skip-gpg-check))))
   (asdf-install:install 		   
@@ -22,12 +21,16 @@
 
 ;;; Define a /hello page
 
-(tpd2:defpage "/hello" (name) :create-frame nil
+(tpd2:defpage "/hello" ((name "Friend")) :create-frame nil
   (tpd2.ml.html:<h1 "Hello " name))
 
 ;;; Start tpd2 listening
 
 (let ((socket (tpd2.io:make-con-listen :port 8080)))
   (tpd2.io:launch-io 'tpd2.io:accept-forever socket 'tpd2.http:http-serve))
+
+;;; Enter the event-loop
+
+(tpd2:event-loop)
 
 ;;; Visit http://localhost:8080/hello
