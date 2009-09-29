53ab6c09b475ac95e5732c71a79378894eebe119
John Fremlin <john@fremlin.org>
2009-09-29 04:42:44 +0000

    print a lot of debugging information about timeouts so that it is clear what is going wrong on mopoko.com

--- src/lib/timeout.lisp
+++ src/lib/timeout.lisp
@@ -34,14 +34,15 @@
 (my-defun timeout position ()
   (quick-queue-get *timeouts* (my time)))
 
+(defun-speedy max-timeout-period ()
+  (length (quick-queue-entries *timeouts*)))
+
 (declaim (inline time-for-delay))
 (defun time-for-delay (delay)
   (declare (optimize speed))
   (let ((delay (floor delay)))
-    (debug-assert (> (length (quick-queue-entries *timeouts*)) (* delay 2)))
+    (debug-assert (> (max-timeout-period) (* delay 2)) (delay))
     (+ (get-timeout-time) delay)))
-
-
 
 (my-defun timeout cancel ()
   (my-declare-fast-inline)
@@ -71,24 +72,52 @@
   (when (my func)
     (funcall (my func))))
 
-(defun next-timeout (&optional (time (get-timeout-time)))
-  (when *timeout-started*
-    (loop for x from *timeout-started* upto time do
-	  (let ((base (quick-queue-get *timeouts* x)))
-	    (loop for cur = (quick-queue-entry-next base)
+(defun describe-timeouts ()
+  (let ((start (or *timeout-started* (- (get-timeout-time) (/ (max-timeout-period) 2)))) (count 0) (earliest nil) (latest nil) (biggest-stack-count 0) biggest-stack-time)
+    (loop for x from start below (+ start (max-timeout-period))
+	  do 
+	  (let ((base (quick-queue-get *timeouts* x)) (stack-count 0))
+	    (loop for cur = (quick-queue-entry-next base) then (quick-queue-entry-next cur)
 		  while (not (eq cur base))
-		  do 
-		  (debug-assert (= (timeout-time cur) x) (cur x))
-		  (timeout-run cur))))
-    (setf *timeout-started* nil))
-  (loop for x from time below (+ time (quick-queue-len *timeouts*))
+		  do (incf count)
+		  (incf stack-count)
+		  (debug-assert (= (timeout-time cur) x) (cur x (timeout-time cur) start))
+		  (unless earliest (setf earliest cur))
+		  (unless latest (setf latest cur))
+		  (when (> (timeout-time cur) (timeout-time latest))
+		    (setf latest cur))
+		  (when (< (timeout-time cur) (timeout-time earliest))
+		    (setf earliest cur)))
+	    (when (> stack-count biggest-stack-count)
+	      (setf biggest-stack-count stack-count
+		    biggest-stack-time x))))
+    (format t "~&Now ~Ds; ~D timeout~:P active.~%" (get-timeout-time) count)
+    (when biggest-stack-time
+      (format t "~&The largest concentration is of ~D timeout~:P in ~Ds.~&" biggest-stack-count (- biggest-stack-time (get-timeout-time)))
+      (format t "~&The next timeout is in ~Ds: ~A~&" (- (timeout-time earliest) (get-timeout-time)) earliest)
+      (describe earliest)
+      (unless (eq latest earliest)
+	(format t "~&The last timeout is in ~Ds: ~A~&" (- (timeout-time latest) (get-timeout-time)) latest)
+	(describe latest)))))
+
+(defun next-timeout (&optional (now (get-timeout-time)))
+  (loop for x from (or *timeout-started* (- now (/ (max-timeout-period) 2))) upto now do
+	(let ((base (quick-queue-get *timeouts* x)))
+	  (loop for cur = (quick-queue-entry-next base)
+		while (not (eq cur base))
+		do 
+		(debug-assert (= (timeout-time cur) x) (cur x now (timeout-time cur) *timeout-started*))
+		(timeout-run cur))))
+  (setf *timeout-started* nil)
+  (describe-timeouts)
+  (loop for x from now below (+ now (max-timeout-period))
 	thereis 
 	(let ((base (quick-queue-get *timeouts* x)))
 	  (let ((timeout (quick-queue-entry-next base)))
 	    (when (not (eq base timeout))
-	      (debug-assert (= (timeout-time timeout) x) (timeout x))
-	      (setf *timeout-started* time)
-	      (- x time))))))
+	      (debug-assert (= (timeout-time timeout) x) (timeout x (timeout-time timeout) now))
+	      (setf *timeout-started* now)
+	      (- x now))))))
 
 (defmacro with-independent-timeouts (() &body body)
   `(let (*timeout-started*
