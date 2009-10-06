61ba6fff2915483684f030c599265f3debec08d4
John Fremlin <john@fremlin.org>
2009-10-06 10:48:42 +0000

    removed parenscript from the addons/ dir as everybody is running their own version anyway

--- addons/parenscript/t/test-package.lisp
+++
@@ -1,8 +0,0 @@
-(in-package :cl-user)
-
-(defpackage :parenscript-test
-  (:nicknames :ps-test :ps-tests :parenscript-tests)
-  (:use :common-lisp :js :5am)
-  (:shadowing-import-from :js :!)
-  (:export #:run-tests
-           #:make-reference-tests-dot-lisp))
