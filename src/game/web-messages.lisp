0c5f65d73307a823781e347350a694790da2c92c
John Fremlin <john@fremlin.org>
2010-02-26 18:33:38 +0000

    tidy up game message code massively

---
+++ src/game/web-messages.lisp
@@ -0,0 +1,77 @@
+(in-package #:tpd2.game)
+
+(defmacro def-web-state-message (message args &body body)
+  `(my-defun web-state 'inform (game-state (message (eql ,message)) &key ,@args &allow-other-keys)
+     (declare (ignorable game-state))
+     (macrolet ((a (&rest args)
+		  `(my add-announcement (<p :class "game-message" ,@args))))
+       ,@body)))
+
+(macrolet ((messages (&body body)
+	     `(progn
+		,@(loop for (keyword args . ml) in body
+			collect `(def-web-state-message ,keyword ,args 
+				   (a ,@ml))))))
+
+  (messages 
+   (:talk (sender text)
+	  (<span :class "game-talk-message" sender ": " (<q text)))
+   (:new-player (player)
+		player " has joined the game.")
+   (:resigned (player)
+	      player " has resigned.")
+   (:timed-out (player)
+	       player " has timed-out.")
+   (:select-card (player choice)
+		 player " played " (output-object-to-ml (make-card-from-number choice)) ".")
+   (:select-demand (player choice)
+		   player " demanded " choice ".")
+   (:select (player choice)
+	    player " chose " (friendly-string choice) ".")
+   (:reject-cards (player choice)
+		  player 
+		  (if choice 
+		      " wants to change cards."
+		      " is satisfied with the cards."))
+   (:accept-new-stake (player choice)
+		  player 
+		  (if choice 
+		      " saw the raise."
+		      " folded."))
+   (:select-new-stake (player choice)
+		  player 
+		  " raised to "
+		  choice
+		  ".")
+   (:winner (player coins)
+	    player " won"
+	    (when coins
+	      (with-ml-output " " coins " coins"))
+	    "."
+	    )
+   (:game-over (winner result)
+	       (cond (winner
+		      (with-ml-output
+			  winner " won the game.")
+		      )
+		     (t
+		      (when result
+			(with-ml-output
+			    (friendly-string result) ".")
+			"Game over."))))
+   (:demand (player amount)
+	    player " demanded " amount ".")
+
+   (:profit (player amount)
+	    player
+	    (if (minusp amount) " lost " " gained ")
+	    (abs amount) ".")
+
+   (:bankrupt (player)
+	      player " went bankrupt.")
+
+   (:betrayal (player)
+	      player " betrayed everybody else.")
+   (:new-state ()
+	       "New game.")))
+
